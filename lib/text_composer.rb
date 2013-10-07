910762346e3b023c92ed65ad172415ff084d2ea6
Andrew8xx8 <avk@8xx8.ru>
2013-10-07 17:09:32 +1100

    Text composer added

---
+++ lib/text_composer.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+class TextComposer
+  class << self
+    def compose(report)
+      report.text = RussianObscenity.sanitize(report.source_text, '')
+
+      via = compose_via report
+
+      _text = report.time.strftime('[%H:%M]') + " {?} ##{report.city.hashtag}#{via}"
+
+      text = report.clean_text.truncate max_text_length(_text)
+
+      _text.gsub(/\{\?\}/, text)
+    end
+
+    def max_text_length(text)
+      143 - text.size
+    end
+
+    def compose_via(report)
+       " via @#{report.sourceable.name}" if report.sourceable.kind_of?(TwitterUser)
+    end
+
+  end
+end
