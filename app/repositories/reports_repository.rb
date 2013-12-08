9f87fd64c7aabde52857cbc294d58a828ab51956
Andreww8xx8 <avk@8xx8.ru>
2013-12-08 10:54:03 +0000

    Scopes fixed

--- app/repositories/reports_repository.rb
+++ app/repositories/reports_repository.rb
@@ -4,15 +4,16 @@ module ReportsRepository
   included do
     include UsefullScopes
 
-    scope :today_posted_in, ->(city) { where(status: :posted).where(city_id: city.id).where('created_at > ? AND created_at < ?', Date.today.beginning_of_day, Time.now).order('created_at DESC') }
     scope :latest_posted, ->() { where(state: :posted).by_time }
+
+    scope :today_posted_in, ->(city) { where(status: :posted).where(city_id: city.id).where('time > ? AND time < ?', Date.today.beginning_of_day, Time.now).order('time DESC') }
 
     scope :this_year, -> { where("time BETWEEN ? AND ?", Date.today.beginning_of_year, Date.today.end_of_year) }
 
     scope :dtp, -> { where(event_kind: :dtp) }
-    scope :by_month, -> { select("date_trunc('month', created_at) AS month , count(*) AS count").group('1').order("month") }
-    scope :by_day, -> { select("to_char(created_at, 'D') AS day , count(*) AS count").group('1').order("day") }
-    scope :by_hour, -> { select("to_char(created_at, 'HH24') AS hour , count(*) AS count").group('1').order("hour") }
+    scope :by_month, -> { select("date_trunc('month', time) AS month , count(*) AS count").group('1').order("month") }
+    scope :by_day, -> { select("to_char(time, 'D') AS day , count(*) AS count").group('1').order("day") }
+    scope :by_hour, -> { select("to_char(time, 'HH24') AS hour , count(*) AS count").group('1').order("hour") }
     scope :in_street, ->(streets) do
       cond = streets.map { |c| "text ILIKE '%#{c}%'" }
       where(cond.join ' OR ')
