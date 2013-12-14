888a041db71f0f4c9bdc24a7cf4cce29c9aebf98
Andreww8xx8 <avk@8xx8.ru>
2013-12-14 12:33:13 +0000

    Issue with wrong redirect fixed

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,19 +1,11 @@
 class Web::ApplicationController < ApplicationController
   protect_from_forgery with: :exception
 
-  before_filter :redirect_if_city_defined!
   before_filter :create_token
 
   def create_token
     return if session.has_key?(:a)
     session[:a] = SecureRandom.hex(8)
-  end
-
-  def redirect_if_city_defined!
-    if session[:city]
-      city = City.friendly.find session[:city]
-      redirect_to city_path(city) if city
-    end
   end
 
 end
--- app/controllers/web/cities_controller.rb
+++ app/controllers/web/cities_controller.rb
@@ -1,7 +1,18 @@
 class Web::CitiesController < Web::ApplicationController
 
+  before_filter :redirect_if_city_defined!
+
   def index
     @cities = City.all
+  end
+
+  private
+
+  def redirect_if_city_defined!
+    if session[:city]
+      city = City.friendly.find session[:city]
+      redirect_to city_path(city) if city
+    end
   end
 
 end
