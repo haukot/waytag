818c3c42dc9276f3731fb7fd3e7369e604ff5d24
Andreww8xx8 <avk@8xx8.ru>
2014-01-27 04:37:53 +0000

    Rss disabled

--- app/controllers/rss/messages_controller.rb
+++ app/controllers/rss/messages_controller.rb
@@ -1,9 +1,13 @@
 class Rss::MessagesController < Rss::ApplicationController
   def feed
-    @city = City.friendly.find params[:id]
-    @messages = @city.reports.latest_posted.limit(20)
+    if params["ololo_yeah!"]
+      @city = City.friendly.find params[:id]
+      @messages = @city.reports.latest_posted.limit(20)
 
-    render :layout => false
-    response.headers["Content-Type"] = "application/xml; charset=utf-8"
+      render :layout => false
+      response.headers["Content-Type"] = "application/xml; charset=utf-8"
+    else
+      render nothing: true, status: :forbidden
+    end
   end
 end
