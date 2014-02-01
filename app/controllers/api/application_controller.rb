380b47f08dc5e0753dba6fd9b9d91075186082b4
Andreww8xx8 <avk@8xx8.ru>
2014-02-01 12:39:30 +0000

    Basic auth added

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,4 +1,6 @@
 class Api::ApplicationController < ApplicationController
+  http_basic_authenticate_with name: "WtRFEf6g6owQofSTWLeg", password: "Etpd1BNhTcfmu3dKP1HT"
+
   respond_to :json
   layout false
 end
